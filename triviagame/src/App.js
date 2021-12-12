15e1b49c07609016d1a8d958c34b774ce9ad530c
SucioLoco <irelandrj@yahoo.com>
2021-12-12 18:46:09 -0500

    categories now render specific category Ids

--- triviagame/src/App.js
+++ triviagame/src/App.js
@@ -6,11 +6,11 @@ import SelectCategory from './SelectCategory.js'
 
 
 let catUrl = "https://opentdb.com/api_category.php"
-let questUrl = "https://opentdb.com/api_count.php?category=${catId}"
+const questUrl = (catId) => `https://opentdb.com/api_count.php?category=${catId}`
 
 export default function App() {
 
-let [category, setCategory] = useState([])
+let [categoryList, setCategoryList] = useState([])
 let [currentCategory, setCurrentCategory] = useState(null)
 
 
@@ -18,13 +18,14 @@ useEffect( () => {
 axios
 .get(catUrl)
 .then((response) => {
-  setCategory(response.data.trivia_categories.map((category) => category.name))
+  setCategoryList(response.data.trivia_categories)
 })
 }, []
 )
+
 function fetchQuestion(catId) {
   axios
-  .get(questUrl)
+  .get(questUrl(catId))
   .then((response) => {
   console.log(response.data)
   })
@@ -33,8 +34,8 @@ function fetchQuestion(catId) {
 return (
   <div>
     <h1>React Trivia!</h1>
-    {category.map((category) => (
-      <SelectCategory name={category.name} key={category.index} id={category.id} setCurrentCategory={setCurrentCategory} fetchQuestion={fetchQuestion}>
+    {categoryList.map((category) => (
+      <SelectCategory name={category.name} key={category.id} id={category.id} setCurrentCategory={setCurrentCategory} fetchQuestion={fetchQuestion}>
       </SelectCategory>
     ))}
   </div>
--- triviagame/src/SelectCategory.js
+++ triviagame/src/SelectCategory.js
@@ -1,8 +1,8 @@
 export default function SelectCategory(props) {
-    let {name, index, id, fetchQuestion, setCurrentCategory } = props
+    let {name, id, fetchQuestion, setCurrentCategory } = props
 
     return (
-        <button key={index} onClick={() => { fetchQuestion(id); setCurrentCategory(id); }}>
+        <button key={id} onClick={() => { fetchQuestion(id); setCurrentCategory(id); }}>
             {name}
         </button>
     )
